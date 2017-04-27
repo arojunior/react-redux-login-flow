10cc0ed169b49af80b506be7811dabc959615ee4
Junior Oliveira <arojunior@gmail.com>
2017-04-27 19:59:21 -0300

    ES6 sintax improvements

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -8,24 +8,22 @@ const setLoginPending = createAction(SET_LOGIN_PENDING)
 const setLoginSuccess = createAction(SET_LOGIN_SUCCESS)
 const setLoginError = createAction(SET_LOGIN_ERROR)
 
-export function login(email, password) {
-  return dispatch => {
-    dispatch(setLoginPending(true))
-    dispatch(setLoginSuccess(false))
-    dispatch(setLoginError(null))
+export const login = (email, password) => dispatch => {
+  dispatch(setLoginPending(true))
+  dispatch(setLoginSuccess(false))
+  dispatch(setLoginError(null))
 
-    callLoginApi(email, password, error => {
-      dispatch(setLoginPending(false))
-      if (!error) {
-        dispatch(setLoginSuccess(true))
-      } else {
-        dispatch(setLoginError(error))
-      }
-    })
-  }
+  callLoginApi(email, password, error => {
+    dispatch(setLoginPending(false))
+    if (!error) {
+      dispatch(setLoginSuccess(true))
+    } else {
+      dispatch(setLoginError(error))
+    }
+  })
 }
 
-function callLoginApi(email, password, callback) {
+const callLoginApi = (email, password, callback) => {
   setTimeout(() => {
     if (email === 'admin@example.com' && password === 'admin') {
       return callback(null)
@@ -35,14 +33,13 @@ function callLoginApi(email, password, callback) {
   }, 1000)
 }
 
-export default function reducer(
-  state = {
-    isLoginSuccess: false,
-    isLoginPending: false,
-    loginError: null
-  },
-  action
-) {
+const initalState = {
+  isLoginSuccess: false,
+  isLoginPending: false,
+  loginError: null
+}
+
+export default (state = initalState, action) => {
   switch (action.type) {
     case SET_LOGIN_PENDING:
       return {
